309e28c13916a13dc7a438586b0eecd7f99cb5c4
Lubov May <liubov.mayorova@gmail.com>
2021-10-05 16:32:59 -0700

    Installed react-router-dom package. Created Nav.js component. Imported Redirect to NewTweet.js. Imported Link, withRouter to Tweet.js. Created Route paths in App.js Component.

--- src/components/NewTweet.js
+++ src/components/NewTweet.js
@@ -1,10 +1,12 @@
 import React, { Component } from 'react'
 import { connect } from 'react-redux'
 import { handleAddTweet } from '../actions/tweets'
+import { Redirect } from 'react-router-dom'
 
 class NewTweet extends Component {
     state = {
         text: '',
+        toHome: false
     }
 
     handleChange = (e) => {
@@ -26,14 +28,17 @@ class NewTweet extends Component {
         console.log('New Tweet:', text)
 
         this.setState(() => ({
-            text: ''
+            text: '',
+            toHome: id ? false : true,
         }))
     }
 
     render() {
-        const { text } = this.state
+        const { text, toHome } = this.state
 
-        {/* todo: Redirect to / if subitted */ }
+        if (toHome === true) {
+            return <Redirect to='/' />
+        }
 
         const tweetLeft = 280 - text.length
 
